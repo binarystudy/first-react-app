7b647de6cc5b86ff1ba6572d16ec9b81e2f6e5ce
binarystudy <66348372+binarystudy@users.noreply.github.com>
2021-09-23 10:28:50 +0530

    First commit to first-react-app

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect, useRef } from 'react';
+import * as mobilenet from "@tensorflow-models/mobilenet";
+function App() {
+  const [isModelLoading, setIsModelLoading] = useState(false)
+  const [model, setModel] = useState(null)
+  const [imageURL, setImageURL] = useState(null);
+  const imageRef = useRef()
+  const textInputRef = useRef()
+  const fileInputRef = useRef()
+  const loadModel = async () => {
+    setIsModelLoading(true)
+    try {
+        const model = await mobilenet.load()
+        setModel(model)
+        setIsModelLoading(false)
+    } catch (error) {
+        console.log(error)
+        setIsModelLoading(false)
+    }
+  }
+
+  const uploadImage = (e) => {
+    const { files } = e.target
+    if (files.length > 0) {
+        const url = URL.createObjectURL(files[0])
+        setImageURL(url)
+    } else {
+        setImageURL(null)
+    }
+  }
+  useEffect(() => {
+        loadModel()
+    }, [])
+  if (isModelLoading) {
+      return <h2>Model Loading...</h2>
+  }
+  
+  return (
+    <div className="App">
+      <h1>Image Recognition React App</h1>
+        <div className='inputHolder'>
+          <input type='file' accept='image/*' capture='camera' className='uploadInput' onChange={uploadImage} ref={fileInputRef} />
+        </div>
+    </div>
+  );
+}
+
+export default App;
